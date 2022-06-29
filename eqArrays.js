5903720c27737f949d7c8965cebfeffe0cf38789
Wesley Wei <wesleytianqiwei@gmail.com>
2022-06-28 21:24:18 -0400

    array comparison

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1,arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      while (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+  
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
