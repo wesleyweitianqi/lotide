f4cddd161f2c5e65efd4b0a25d9f5c5904c66d63
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-02 11:35:27 -0400

    N-lever object compare

--- eqObjects.js
+++ eqObjects.js
@@ -13,6 +13,7 @@ const eqArrays = function(arr1,arr2) {
     for (let i = 0; i < arr1.length; i++) {
       while (arr1[i] !== arr2[i]) {
         return false;
+      
       }
     }
     return true;
@@ -21,37 +22,50 @@ const eqArrays = function(arr1,arr2) {
 const eqObjects = function(object1, object2) {
   //have same number of keys;
   const keys1 = Object.keys(object1);
+  console.log(keys1)
   const keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) {
     return false;
   }
+  
   for (let key of keys1) {
+    console.log(key);
     let value1 = object1[key];
     let value2 = object2[key];
     console.log(value1,value2);
-    if (!eqArrays(value1,value2)) {
-      return false;
+    if (typeof(value1) === typeof(value2) && typeof(value1) === 'object') {
+      return eqObjects(value1,value2);
     }
+    if (typeof(value1) !== typeof(value2)) return false;
+    if (!eqArrays(value1,value2)) return false;
+    if (!assertEqual(value1,value2)) return false;
   }
   return true;
+  
 };
   
 
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+//console.log(eqObjects(ab, ba)); // => true
 assertEqual(eqObjects(ab, ba), true);
 console.log('////////////////////');
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+//console.log(eqObjects(ab, abc)); // => false
 assertEqual(eqObjects(ab, abc), false);
 console.log('////////////////////');
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
 assertEqual(eqObjects(cd, dc), true); // => true
+console.log('////////////////////');
+
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2),false); // => false
+console.log('////////////////////');
 
 
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));// => true
 
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }));// => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }));// => false
