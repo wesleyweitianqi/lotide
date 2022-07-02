2ca992a8df5fe95566c3c3002f7e27f175e1ec14
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-02 19:17:30 -0400

    N-lever array compare

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,24 @@
 const eqArrays = function(arr1,arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      while (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
+  let result = true;
+  if (arr1.length !== arr2.length) {
+    result = false;
+    
   }
+  
+  for (let i in arr1) {
+    
+    if (typeof (arr1[i]) !== typeof (arr2[i])) {
+      result = false;
+     
+    }
+      
+    if (arr1[i] instanceof Array && arr2[i] instanceof Array) {
+      console.log(eqArrays(arr1[i], arr2[i]));
+    } else if (arr1[i] !== arr2[i]) result = false;
+    
+    
+  }
+  return result;
 };
 
 const assertEqual = function(actual, expected) {
@@ -19,3 +31,6 @@ const assertEqual = function(actual, expected) {
   
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);// => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), true);
