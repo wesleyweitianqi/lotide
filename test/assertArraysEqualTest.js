5dc21cdeec3b856285c4c5fbfdf76a62131eaf93
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-04 14:04:10 -0400

    asserArrayEqualTest update

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,7 +2,7 @@
 const assertArraysEqual = require('../assertArraysEqual');
 
 
-const result = assertArraysEqual([1,2],[1,2,3]);
-const result1 = assertArraysEqual([1,2],[1,3]);
-const result2 = assertArraysEqual([1,2],[1,2]);
+assertArraysEqual([1,2],[1,2,3]);
+assertArraysEqual([1,2],[1,3]);
+assertArraysEqual([1,2],[1,2]);
 
