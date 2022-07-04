7c0ea813b829543a7f29018d272e7b40e8483208
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-04 15:48:51 -0400

    refactor tail by using mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,27 @@
-const assertEqual = require('../assertEqual');
-const tail  = require('../tail');
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const tail  = require('../tail');
+const assert = require('chai').assert;
+
+describe('#tail', () => {
+  it('should return original array element numbers', () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.equal(words.length, 3);
+  });
+
+  it('shoudl return empty array if an Array with only one element', () => {
+    const words = [2];
+    tail(words);
+    assert.equal(words.length, 1);
+    assert.equal(tail(words).length,2);
+  });
+});
+
+
+
+
+
+// // Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
