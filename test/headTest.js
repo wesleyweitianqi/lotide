bec78860ce2166bf77ca40bfba0fe98738430b4b
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-04 14:47:48 -0400

    use mocha and chai and convert headTest

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,33 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head');
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 8);
+const assert = require('chai').assert;
+const head   = require('../head');
+
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("return 'Hello' for `['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello');
+  });
+
+  it('should return 8 for [5,6,7]', ()=>{
+    assert.strictEqual(head([5,6,7]), 8);
+  });
+});
+
+
+
+
+
+
+
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 8);
