4f3f45dbc9057cab6a2be95c8c43d260d2d8cc18
Wesley Wei <wesleytianqiwei@gmail.com>
2022-07-04 13:28:53 -0400

    seperate tail test

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual.toString() !== expected.toString()) {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  } else {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-};
 
 const tail = function(arr) {
   if (arr.length <= 1) {
@@ -15,9 +7,5 @@ const tail = function(arr) {
   }
 };
 
+module.exports = tail;
 
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
